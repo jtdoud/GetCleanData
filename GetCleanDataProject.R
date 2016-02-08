fd87c24b4c32fc51a225ae6df4faecad4cf36e7e
jtdoud <jtdoud@gmail.com>
2016-02-08 15:39:49 -0800

    Removed instructions

--- GetCleanDataProject.R
+++ GetCleanDataProject.R
@@ -1,25 +1,6 @@
 
-# ------------------------------------------------------
 # Getting and Cleaning Data Course Project
-# Tucker Doud
-
-# Review Criteria
-
-# The submitted data set is tidy.
-# The Github repo contains the required scripts.
-# GitHub contains a code book that modifies and updates the available codebooks with the data to indicate all the variables and summaries calculated, along with units, and any other relevant information.
-# The README that explains the analysis files is clear and understandable.
-# The work submitted for this project is the work of the student who submitted it.
-
-# Instructions
-# You should create one R script called run_analysis.R that does the following.
-
-# 1. Merges the training and the test sets to create one data set.
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-# 3. Uses descriptive activity names to name the activities in the data set
-# 4. Appropriately labels the data set with descriptive variable names.
-# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-# ----------------------------------------------------------
+# Created by: Tucker Doud
 
 library(dplyr)
 setwd("~/GitHub/GetCleanData")
